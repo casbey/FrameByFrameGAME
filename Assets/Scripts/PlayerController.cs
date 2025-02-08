38673ed316ea63f54dc26fd62eb197f839284d30
Pipa Csaba <csabapipa@gmail.com>
2025-02-08 16:01:38 +0100

    Making the basic movements
    -Added a test level background.
    -Added a part of the idle animation as a PlayerSprite.
    -Created the script for jumping, and moving to sides.
    -Added box colliders, and a groundcheck so that the character can only jump when he is on the ground.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public float speed;
+    public float firstJumpAmount;
+    float inputMovement;
+
+    Rigidbody2D rb;
+
+    bool isOnGround;
+    public Transform groundCheck;
+    public LayerMask GroundMask;
+
+    public float radius;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        isOnGround = Physics2D.OverlapCircle(groundCheck.position, radius, GroundMask);
+
+        inputMovement = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(inputMovement * speed, rb.linearVelocity.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isOnGround && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.linearVelocity = Vector2.up * firstJumpAmount;
+        }
+    }
+}
